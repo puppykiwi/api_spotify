9280ef5eacc49b3356e29b62477ef8ebec0458ad
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-28 12:44:49 +0300

    id bum[p

--- recommend.py
+++ recommend.py
@@ -49,7 +49,8 @@ def get_playlist_id(playlist_name, token):
 if __name__ == "__main__":
     print("Running recommend.py")
     token = get_token()
-    id = get_playlist_id("Indie Infusion", token)
-    pl = Playlist(id, token)
+    indie_id = "7eG04lBozqMlzgmpM1omp3"
+    #id = get_playlist_id(input("Enter playlist name: "), token)
+    pl = Playlist(indie_id, token)
     result = pl.print_playlist_info()
-    pl.get_playlist_tracks()
+    pl.print_playlist_tracks()
