0657b700dca3d6ca7a9d12230ef2928899740ab1
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-28 13:20:25 +0300

    skin

--- playlist.py
+++ playlist.py
@@ -5,33 +5,7 @@ import requests
 import base64
 import json
 import random
-
-###
-from dotenv import load_dotenv
-load_dotenv()
-id = os.getenv("CLIENT_ID")
-secret = os.getenv("CLIENT_SECRET")
-def get_token():
-    auth = base64.b64encode(bytes(id + ":" + secret, "utf-8")).decode("ascii")
-
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + auth,
-        "Content-Type": "application/x-www-form-urlencoded"
-        }
-    data = {"grant_type": "client_credentials"}
-
-    response = requests.post(url, headers=headers, data=data)
-    token = json.loads(response.content)["access_token"]
-
-    return token
-
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
-
-
-###
-
+from init import get_token, get_auth_header
 
 """  Playlist class  """
 
