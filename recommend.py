4a4a1f45ed29ce0984af88b2dd6befe9090741c7
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-28 11:52:42 +0300

    get_playlist_id

--- recommend.py
+++ recommend.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 # A better recommendation engine 
-# This is a work in progress
 
 from dotenv import load_dotenv
 import os
@@ -9,6 +8,7 @@ import requests
 import base64
 import json
 import random
+from playlist import Playlist
 
 load_dotenv()
 id = os.getenv("CLIENT_ID")
@@ -40,37 +40,32 @@ def get_playlist_id(playlist, token):
     result = json.loads(response.content)
     return result
 
+def get_playlist_id(playlist_name, token):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    params = {"q": playlist_name, "type": "playlist"}
+    response = requests.get(url, headers=headers, params=params)
+    result = json.loads(response.content)
+    playlists = result.get("playlists", {}).get("items", [])
+    if playlists:
+        return playlists[0]["id"]  # Return the first playlist ID found (assumes unique names)
+    return None
 
-"""  Playlist class"""
-
-class Playlist:
-    params = {"country": "KE"}
-    
-    def __init__(self, playlist_id, token):
-        self.playlist_id = playlist_id
-        self.token = token
-        self.base_url = f"https://api.spotify.com/v1/playlists/{self.playlist_id}"
-    
-    def get_playlist_tracks(self):
-        url = self.base_url + "/tracks"
-        headers = get_auth_header(self.token)
-        result = self.get_request(url)
-        for idx in result["items"]:
-            print(idx["track"]["name"])
-
-    def get_request(self, custom_url=None):
-        headers = get_auth_header(self.token)
-
-        try:
-            url = custom_url if custom_url else self.base_url
-            response = requests.get(url, headers=headers, params=self.params)
-            response.raise_for_status()  # Raise an exception for any HTTP error status
-            return json.loads(response.content)
-        except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
-            return None
 
 if __name__ == "__main__":
+    print("Running recommend.py")
     token = get_token()
-    pl = Playlist("7eG04lBozqMlzgmpM1omp3", token)
-    tracks = pl.get_playlist_tracks()
+    print("token: ",token)
+    id = get_playlist_id("S+Puppy", token)
+    print("id: ",id)
+    pl = Playlist(id, token)
+    if pl:
+        print("Playlist object created")
+    else:
+        print("Error creating Playlist object")
+    
+    result = pl.get_playlist_info()
+    print(result["id"])
+    print(result["name"])
+    print(result["description"])
+    pl.get_playlist_tracks()
